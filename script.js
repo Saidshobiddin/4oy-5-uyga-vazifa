daa4bca2768217380d9bd5c887f95a896e0cad60
unknown <saidshohobiddinmansurov75@gmail.com>
2024-03-08 11:27:15 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let a = prompt("Ismizni  kiriting");
+let b = prompt("bronta harf kiriting") 
+if(a){
+    alert("bu harf bor")  
+    alert(a.toUpperCase("Saidshahobiddin"));
+   
+}
+else if (b){
+   
+    alert("bu harf yoq")
+   alert(b.toUpperCase("Saidshahobiddin"))
+}
+
